d3057d7695efd0f368ce08680ee66e80ad699d3a
kasinaat.ss <kasinaat.ss@zohocorp.com>
2022-02-20 17:43:52 +0530

    Third Commit

--- Solution.java
+++ Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public static void main(String args[]) {
-        System.out.println("Hello Java Batchmate " + args[0]);
+        System.out.println("Hello Java Batch!");
     }
 }
 //v1.

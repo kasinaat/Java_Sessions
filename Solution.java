fdce5fd305f6775b73e75d7b4e9bc4cb8761e423
kasinaat.ss <kasinaat.ss@zohocorp.com>
2022-02-20 17:40:44 +0530

    First commit

---
+++ Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void main(String args[]) {
+        System.out.println("Hello Java Batchmate " + args[0]);
+    }
+}
+//v1.2
+
+//Version Control Systems - VCS
+
+//Git - Linus Torvolds
